cd9d889c071dab0eba04952f9be7ed1d68723fdf
SphericalKat <amolele@gmail.com>
2020-11-05 13:30:48 +0530

    :zap: wire up URL routing and views
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- template/api/views.py
+++ template/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import NoteSerializer
+from .models import Note
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all().order_by('title')
+    serializer_class = NoteSerializer
