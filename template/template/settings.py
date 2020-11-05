88ac708899b6837fc1592d70cc836966eda84b3a
SphericalKat <amolele@gmail.com>
2020-11-05 13:49:21 +0530

    :books: add swagger doc generators
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- template/template/settings.py
+++ template/template/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
 
 ROOT_URLCONF = 'template.urls'
 
