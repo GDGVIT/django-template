2b4bf71940dec4374a6d1055cafc281baad48d23
SphericalKat <amolele@gmail.com>
2020-11-05 12:49:27 +0530

    :sparkles: add note model and register it with admin
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- template/api/models.py
+++ template/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
